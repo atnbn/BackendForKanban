6ff0b5dc3b278ab2b43a2907a305a6d2c266b017
atnbn <altan.bnm@gmail.com>
2024-04-26 22:22:02 +0200

    qee

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(session({
     }
 }))
 
-// app.set("trust proxy", 1);
+app.set("trust proxy", 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', authRoutes);
